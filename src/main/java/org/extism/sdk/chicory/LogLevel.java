e6a16d4763ce176443eff944c09fd607d6c86c6c
Edoardo Vacchi <evacchi@users.noreply.github.com>
2025-01-10 10:46:33 +0100

    feat: missing env get_log_level (#31)
    implement the missing API and expose it. Currently this
    uses the SystemLogger from Chicory. It might make sense
    to instead provide an sdk-specific implementation
    
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

--- src/main/java/org/extism/sdk/chicory/LogLevel.java
+++ src/main/java/org/extism/sdk/chicory/LogLevel.java
@@ -2,6 +2,8 @@ package org.extism.sdk.chicory;
 
 import com.dylibso.chicory.log.Logger;
 
+import java.util.logging.Level;
+
 public enum LogLevel {
     TRACE, DEBUG, INFO, WARN, ERROR;
 
@@ -19,6 +21,38 @@ public enum LogLevel {
                 return Logger.Level.ERROR;
         }
         throw new IllegalArgumentException("unknown type " + this);
+    }
+
+    Level toJavaLogLevel() {
+        switch (this) {
+            case TRACE:
+                return Level.FINEST;
+            case DEBUG:
+                return Level.FINE;
+            case INFO:
+                return Level.INFO;
+            case WARN:
+                return Level.WARNING;
+            case ERROR:
+                return Level.SEVERE;
+        }
+        throw new IllegalArgumentException("unknown type " + this);
+    }
+
+    public static LogLevel fromJavaLogLevel(Level level) {
+        switch (level.getName()) {
+            case "FINEST":
+                return TRACE;
+            case "FINE":
+                return DEBUG;
+            case "INFO":
+                return INFO;
+            case "WARNING":
+                return WARN;
+            case "SEVERE":
+                return ERROR;
+        }
+        throw new IllegalArgumentException("unknown type " + level.getName());
     }
 }
 
